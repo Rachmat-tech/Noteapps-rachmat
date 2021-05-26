3db3d56ed6615f7f00c7bef5e7c1f1e0dcd971ad
Rachmat_tech <maulanarachmat357@gmail.com>
2021-05-26 18:20:01 +0800

    last fix

--- app/src/main/java/id/ac/rachmatmaulana/noteapp/AppDatabase.kt
+++ app/src/main/java/id/ac/rachmatmaulana/noteapp/AppDatabase.kt
@@ -6,10 +6,8 @@ import android.provider.ContactsContract
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.belajarroom.db.note.Note
-import com.example.belajarroom.db.note.NoteDao
 
-@Database(entities = [ContactsContract.CommonDataKinds.Note::class], exportSchema = false, version = 1)
+@Database(entities = [Note::class], exportSchema = false, version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun noteDao(): NoteDao
